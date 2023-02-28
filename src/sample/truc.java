d23b0d1444c6eef48028d0e242c0dbb614b957fb
r.palayret <palayrer@3il.fr>
2023-02-28 10:03:13 +0100

    Initial Commit

---
+++ src/sample/truc.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.scene.paint.Color;
+
+public final class truc {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
