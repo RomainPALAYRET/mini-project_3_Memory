9bbe49e7ba8009102a8f091da205dc7b113c5d66
r.palayret <palayrer@3il.fr>
2023-03-02 15:16:29 +0100

    Add Img Loader

--- src/sample/controler/Controller.java
+++ src/sample/controler/Controller.java
@@ -1,7 +1,70 @@
 package sample.controler;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import sample.model.Carte;
 import javafx.beans.Observable;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    @FXML
+    private Button bStart;
+
+    @FXML
+    private GridPane gridPaneCenter;
+
+    @FXML
+    private void actionBStart(javafx.event.ActionEvent evt) {
+
+        System.out.println("Carapuce");
+        List<Carte> lCarte = createListCarte();
+
+        /* On creer une grille de 6 par 4 qu'on place au centre du BorderPane */
+        gridPaneCenter.getColumnConstraints().clear();
+        gridPaneCenter.getRowConstraints().clear();
+
+        for(int i = 1; i < 4; i ++) {
+            gridPaneCenter.getColumnConstraints().add(new ColumnConstraints());
+        }
+
+        for(int j = 1; j < 6; j ++){
+            gridPaneCenter.getRowConstraints().add(new RowConstraints());
+        }
+
+        /* On rempli toutes les cases par des bouttons avec des images de "?" */
+        for(int i = 1; i < 4; i ++) {
+            for (int j = 1; j < 6; j++) {
+                //final int nbAlea = (int)(Math.random() * ((lCarte.size())));
+                ImageView n = new ImageView();
+                n.setImage(new Image("file:///C:/Users/r.palayret/IdeaProjects/Memory/ressource/img/Stade1_001.png"));
+                gridPaneCenter.add(n, i, j);
+                System.out.println(n);
+                //gridPaneCenter.add(new ImageView(lCarte.get(nbAlea).getFace()), i, j);
+                //lCarte.remove(nbAlea);
+            }
+        }
+
+
+        /* On lance le chrono */
+    }
+
+    private List<Carte> createListCarte() {
+        List lCarte = new ArrayList<Carte>();
+        for(int i = 1; i < 12; i ++) {
+            final String path = "Stade1_00" + i;
+            lCarte.add(new Carte(path));
+            lCarte.add(new Carte(path));
+        }
+        return lCarte;
+    }
 
 }
--- src/sample/model/Carte.java
+++ src/sample/model/Carte.java
@@ -4,6 +4,8 @@
  */
 package sample.model;
 
+import javafx.scene.image.Image;
+
 /**
  * Une carte d'un jeu mémory, cette carte possède une image qui la définie
  * @author Palayret Romain
@@ -12,12 +14,19 @@ package sample.model;
 public class Carte {
 
     private String path; // le chemin de l'image sur la carte;
+
+    private Image face;
+
     /**
      * Constructeur de la classe Carte
      * @param path le chemin de l'image
      */
     public Carte(String path) {
+
+        String truePath = "file:ressource/img/" + path;
+
         this.path = path;
+        face = new Image(truePath);
     }
 
     /**
@@ -25,6 +34,13 @@ public class Carte {
      */
     public String getPath() {
         return this.path;
+    }
+
+    /**
+     * @return l'image de la carte
+     */
+    public Image getFace() {
+        return this.face;
     }
 
     /**
